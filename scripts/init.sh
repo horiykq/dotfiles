40d5a2901316d0efd266042567c1bbd99b21d53c
horiy0125 <k.horiy0125@gmail.com>
2023-04-07 11:36:03 +0900

    feat: update scripts/init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 
 echo '    __               _       ____ ______   ______               '
 echo '   / /_  ____  _____(_)_  __/ __ <  /__ \ / ____/               '
@@ -18,52 +18,18 @@ echo ""
 WORKSPACE="$HOME/workspace"
 DOTFILES="$WORKSPACE/dotfiles"
 
-ZSHRC="$HOME/.zshrc"
-CONFIG="$HOME/.config"
-GITCONFIG="$HOME/.gitconfig"
-SSH="$HOME/.ssh"
-SSHCONFIG="$HOME/.ssh/config"
-
-PYENV="$HOME/.pyenv"
-NODENV="$HOME/.nodenv"
-GOENV="$HOME/.goenv"
-RBENV="$HOME/.rbenv"
-
-NODE_BUILD="$NODENV/plugins/node-build"
-RUBY_BUILD="$RBENV/plugins/ruby-build"
-
 echo ""
-echo "=== 1. Homebrew のインストール ==="
-echo ""
-
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-echo ""
-echo "=== 1. END Homebrew のインストール ==="
-echo ""
-
-echo ""
-echo "=== 2. Ansible のインストール"
-echo ""
-
-brew install ansible
-
-echo ""
-echo "=== 2. END Ansible のインストール"
-echo ""
-
-echo ""
-echo "=== 3. $WORKSPACE ディレクトリの作成 ==="
+echo "=== 1. $WORKSPACE ディレクトリの作成 ==="
 echo ""
 
 mkdir -p "$WORKSPACE"
 
 echo ""
-echo "=== 3. END $WORKSPACE ディレクトリの作成 ==="
+echo "=== 1. END $WORKSPACE ディレクトリの作成 ==="
 echo ""
 
 echo ""
-echo "=== 4. dotfiles リポジトリのクローン ==="
+echo "=== 2. dotfiles リポジトリのクローン ==="
 echo ""
 
 if [ -d "$DOTFILES" ]; then
@@ -73,5 +39,5 @@ else
 fi
 
 echo ""
-echo "=== 4. END dotfiles リポジトリのクローン ==="
+echo "=== 2. END dotfiles リポジトリのクローン ==="
 echo ""
